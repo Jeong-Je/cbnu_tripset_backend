e494639422e0c74df42fda94ceae706deb5a0ac9
Jeong-Je <jeongjeson656@gmail.com>
2024-04-11 23:44:53 +0900

    페이지네이션을 위한 임시 게시글 생성 코드 🚀

--- backend/src/test/java/com/trip/Let/sGo/ApplicationTests.java
+++ backend/src/test/java/com/trip/Let/sGo/ApplicationTests.java
@@ -1,13 +1,33 @@
 package com.trip.Let.sGo;
 
+import com.trip.Let.sGo.post.entity.PostEntity;
+import com.trip.Let.sGo.post.repository.PostRepository;
+import com.trip.Let.sGo.user.entity.UserEntity;
+import com.trip.Let.sGo.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private PostRepository postRepository;
+
 	@Test
 	void contextLoads() {
+		UserEntity user = this.userRepository.findByUsername("bot");
+
+		for(int i=1;i<=300;i++){
+			PostEntity post = new PostEntity();
+			post.setAuthor(user);
+			post.setContent(String.format("[%03d] > 대용량 게시글 테스트", i));
+			post.setTitle(String.format("[%03d] > 대용량 게시글 테스트", i));
+
+			postRepository.save(post);
+		}
 	}
 
 }
